4b1dabdc0922371b0c994c0ebd55995efa56fd67
Shubham <shubhampatel518@gmail.com>
2018-10-21 03:25:34 +0530

    rendering content and outputing list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -39,15 +39,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
     let persons = null;
+    let buttonClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -62,25 +55,24 @@ class App extends Component {
           }) }
         </div>
       );
-
-      style.backgroundColor = 'red';
+      buttonClass = classes.Red;
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-      <div className="App">
+      <div className={ classes.App }>
         <h1>Hi, This is Shubham</h1>
-        <p className={ classes.join(' ') }>This is working</p>
+        <p className={ assignedClasses.join(' ') }>This is working</p>
         <button
-        style={ style }
-        onClick={ this.togglePersonsHandler }>Toggle Person</button>
+          className={ buttonClass }
+          onClick={ this.togglePersonsHandler }>Toggle Person</button>
         { persons }
       </div>
     );
