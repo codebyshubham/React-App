f166f938c08656a97c512238178a6a47ed6aad08
Shubham <shubhampatel518@gmail.com>
2018-10-28 01:40:07 +0530

    Truly higher order function
    truly higher order function

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -88,7 +89,7 @@ class App extends PureComponent {
 
 
     return (
-      <WithClass classes={ classes.App }>
+      <Aux>
         <button onClick={() => { this.setState({ showPersons: true }) }}>Show Persons</button>
         <Cockpit 
           appTitle={ this.props.title }
@@ -96,9 +97,9 @@ class App extends PureComponent {
           persons={ this.state.persons }
           clicked={ this.togglePersonsHandler }/>
         { persons }
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
